8638a7b6825310d16fee08a71fc53eb6aae3c8b9
rdk22 <ufixmakrela@gmail.com>
2021-09-24 08:54:42 +0000

    pierwszy

---
+++ Math.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace MathSolution
+{
+    class Math
+    {
+        public int Add(int a, int b)
+        {
+            return a+b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a-b;
+        }
+        public double Div(int a, int b)
+        {
+            if(b==0)
+            throw new DivideByZeroException();
+            return (double)a/(double)b;
+        }
+        public int Mul(int a, int b)
+        {
+            return a*b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MathSolution
+{
+    class Program
+    {
+        private Math math= new Math();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1+1="+math.Add(1,1));
+        }
+    }
+}
