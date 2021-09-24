e831b61aa4bf321fe0d3a96174a00a04352afb8d
rdk22 <ufixmakrela@gmail.com>
2021-09-24 11:55:29 +0000

    piec

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace MathSolution
         public Math math= new Math();
         static void Main(string[] args)
         {
-            Console.WriteLine("1 + 1 = ");
+            Console.WriteLine("1 + 1 = 2");
         }
     }
 }
